6337a28b8499c664e1f10acca9e06abed834b9af
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-11-06 22:46:34 +0100

    Add project files.

---
+++ D2Calculator/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace D2Calculator.D2Calculator
+{
+	public class Calculator
+	{
+		private readonly double _bodyDamage;
+		private readonly double _critDamage;
+
+		public Calculator(double baselineBodyDamage, double baselineCritDamage, params double[] multipliers)
+		{
+			if (multipliers.Length > 2)
+			{
+				throw new ArgumentException("Cannot apply more than two multipliers", nameof(multipliers));
+			}
+
+			this._bodyDamage = baselineBodyDamage;
+			this._critDamage = baselineCritDamage;
+
+			if (multipliers?.Length != 0)
+			{
+				foreach (var multiplier in multipliers)
+				{
+					this._bodyDamage *= multiplier;
+					this._critDamage *= multiplier;
+				}
+			}
+		}
+
+		public int GetTotalShots(int health)
+			=> (int)Math.Round(health / this._critDamage, 0, MidpointRounding.ToPositiveInfinity);
+
+		public int GetBodyShots(int health)
+			=> GetBodyShots(GetTotalShots(health), health);
+
+		public int GetBodyShots(int totalShots, int health)
+		{
+			if (this._bodyDamage * totalShots >= health)
+			{
+				return totalShots;
+			}
+			else
+			{
+				var critsToKill = (int)Math.Round(health / this._critDamage, MidpointRounding.ToPositiveInfinity);
+				return (int)Math.Round(((critsToKill * this._critDamage) - health) / (this._critDamage - this._bodyDamage), 0, MidpointRounding.ToNegativeInfinity);
+			}
+		}
+
+		public int GetCritShots(int health)
+			=> GetCritShots(GetTotalShots(health), GetBodyShots(health));
+
+		public int GetCritShots(int totalShots, int bodyShots)
+			=> totalShots - bodyShots;
+	}
+}
---
+++ D2CalculatorCockpit/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Windows;
+using System.Windows.Documents;
+using D2Calculator.D2Calculator;
+
+namespace D2CalculatorCockpit
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly int[] resils = new int[11]
+		{
+			185, 186, 187, 188, 189, 190, 192, 194, 196, 198, 200
+		};
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.Plot.Plot.Clear();
+
+			var calculator = new Calculator(int.Parse(this.bodyDmgSelector.Text), int.Parse(this.critDmgSelector.Text));
+
+			var bodyShots = new double[11];
+			var critShots = new double[11];
+
+
+			for (var i = 0; i < 11; i++)
+			{
+				bodyShots[i] = calculator.GetBodyShots(this.resils[i]);
+				critShots[i] = calculator.GetCritShots(this.resils[i]);
+			}
+
+			for (var i = 0; i < bodyShots.Length; i++)
+			{
+				bodyShots[i] += critShots[i];
+			}
+
+			var bar = this.Plot.Plot.AddBar(bodyShots);
+			bar.ShowValuesAboveBars = true;
+			bar.Label = "Bodyshots";
+			bar = this.Plot.Plot.AddBar(critShots);
+			bar.Label = "Critical hits";
+
+			_ = this.Plot.Plot.XAxis.Label("Resilience Level");
+			_ = this.Plot.Plot.YAxis.Label("Shots needed");
+
+			var axispos = new List<double>();
+			var axislabels = new List<string>();
+			for (var i = 0; i < bodyShots.Last() + 1; i++)
+			{
+				axispos.Add(i);
+				axislabels.Add(i.ToString());
+			}
+			this.Plot.Plot.YAxis.ManualTickPositions(axispos.ToArray(), axislabels.ToArray());
+
+			axispos = new();
+			axislabels = new();
+			for (int i = 0; i < 11; i++)
+			{
+				axispos.Add(i);
+				axislabels.Add($"Resil {i}");
+			}
+			this.Plot.Plot.XAxis.ManualTickPositions(axispos.ToArray(), axislabels.ToArray());
+
+			this.Plot.Plot.SetAxisLimits(yMin: 0);
+			_ = this.Plot.Plot.Legend();
+
+			this.Plot.Refresh();
+		}
+	}
+}
